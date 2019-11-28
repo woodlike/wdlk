242b942ccf1aec79cfaeae62c3f89f8c41c21fae
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2019-11-28 10:44:06 +0100

    chore(theme-query): update font family names

--- packages/theme-query/src/tokens/fonts/familiy.ts
+++ packages/theme-query/src/tokens/fonts/familiy.ts
@@ -1,9 +1,9 @@
 import { FontKey, FontValue } from '../fonts';
 
 export const fontFamily: Record<FontKey, FontValue> = {
-  body: `"museo_sans300", Helvetica, sans-serif`,
+  body: `"MuseoSans", Helvetica, sans-serif`,
   heading: {
-    display: `"museo300", serif`,
-    secondary: `"museo500", serif`,
+    display: `"Museo", serif`,
+    secondary: `"Museo500", serif`,
   },
 };
