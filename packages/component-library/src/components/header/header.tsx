067a4a14f2791d1d78102a67edf877c399666098
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2019-12-04 18:43:42 +0100

    feat(component-library): include header component

--- packages/component-library/src/components/header/header.tsx
+++ packages/component-library/src/components/header/header.tsx
@@ -6,10 +6,18 @@ import { qt } from '../query';
 
 const styledHeader: SxStyleProp = {
   display: 'grid',
-  gridTemplateColumns: '1fr 6fr 1fr',
+  gridTemplateColumns: '1fr 5fr 1fr',
   gridColumnGap: `${qt('spaces')(3)}px`,
   alignItems: 'center',
+  boxSizing: 'border-box',
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  width: '100%',
   padding: `${qt('spaces')(2)}px ${qt('spaces')(4)}px`,
+  borderBottom: `solid 1px ${qt('grays')(0)}`,
+  backgroundColor: `${qt('whites')(0)}`,
+  zIndex: 1,
 };
 
 const styledHeaderItem: SxStyleProp = {
