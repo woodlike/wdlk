e9e52282d60d54d93c4d13b412e6eca3c53a62a3
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2019-12-21 19:48:21 +0100

    docs(component-library): release

--- packages/component-library/src/components/index.ts
+++ packages/component-library/src/components/index.ts
@@ -5,5 +5,4 @@ export * from './navigation';
 export * from './navigation-bar';
 export * from './navigation-link';
 export * from './navigation-panel';
-export * from './query';
 export * from './with-focus-style';
--- packages/component-library/src/components/query.ts
+++
@@ -1,4 +0,0 @@
-// import * as ThemeQuery from 'theme-query';
-// import { theme } from 'gatsby-theme-query';
-
-// export const qt = ThemeQuery.create({ theme, styles: 'object' });
