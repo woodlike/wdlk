95c3bceba6571afdb47fe1ecc2585d9183e77f27
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-03-07 13:57:06 +0100

    feat(components): expand docs logo implementation

--- packages/components/src/docs/logo.tsx
+++ packages/components/src/docs/logo.tsx
@@ -2,19 +2,70 @@
 import { jsx, SxStyleProp } from 'theme-ui';
 
 export interface DocsLogoProps {
-  path: SVGPathElement;
-  minX: number;
-  minY: number;
-  width: number;
-  height: number;
+  readonly size: LogoSize;
+  readonly path: JSX.Element;
+  readonly minX: number;
+  readonly minY: number;
+  readonly width: number;
+  readonly height: number;
+  readonly title: string;
+  readonly desc: string;
+  readonly base?: BaseSize;
+  readonly isFocused?: boolean;
 }
+
+export interface BaseSize {
+  readonly width: number;
+  readonly height: number;
+}
+
+export type LogoSize = 'S' | 'M' | 'L' | 'XL';
+
+export const intBaseSize = {
+  width: 185,
+  height: 42,
+};
 
 const stylesDocsLogo: SxStyleProp = {
   color: 'primary',
 };
 
-export const DocsLogo: React.FC<DocsLogoProps> = (props): JSX.Element => (
-  <svg sx={stylesDocsLogo} viewBox={`${props.minX} ${props.minY} ${props.width} ${props.height}`}>
+const createLogoSizes = (size: LogoSize, base: BaseSize): SxStyleProp => {
+  switch (size) {
+    case 'S':
+      return base;
+    case 'M':
+      return {
+        width: base.width * 2,
+        height: base.height * 2,
+      };
+    case 'L':
+      return {
+        width: base.width * 3,
+        height: base.height * 3,
+      };
+    case 'XL':
+      return {
+        width: base.width * 4,
+        height: base.height * 4,
+      };
+  }
+};
+
+const createLogoStyles = (size: LogoSize, base: BaseSize = intBaseSize): SxStyleProp => ({
+  ...stylesDocsLogo,
+  ...createLogoSizes(size, base),
+});
+
+export const Logo: React.FC<DocsLogoProps> = (props): JSX.Element => (
+  <svg
+    sx={createLogoStyles(props.size, props.base)}
+    aria-labelledby={`docs-logo-${props.title.replace(/ /g, '')}`}
+    viewBox={`${props.minX} ${props.minY} ${props.width} ${props.height}`}>
+    <title id={`docs-logo-${props.title.replace(/ /g, '')}`}>{props.title}</title>
+    <desc>{props.desc}</desc>
     {props.path}
   </svg>
 );
+
+Logo.displayName = 'Docs.Logo';
