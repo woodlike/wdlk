2554736fc9e29f6b4b1e19448efbb4fb2346743e
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-03-07 14:55:35 +0100

    feat(docs): include logo component

---
+++ packages/docs/src/gatsby-theme-docz/components/Logo/index.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Link } from 'docz';
+import { Docs } from '@wdlk/components';
+
+export const Logo = (): JSX.Element => {
+  return (
+    <Link to="/">
+      <Docs.Logo
+        minX={0}
+        minY={0}
+        width={92}
+        height={46}
+        size="S"
+        title="Woodlike Docs"
+        desc="Discover the guidelines and thought processes behind our APIs and design system approach."
+        path={<Docs.Path />}
+      />
+    </Link>
+  );
+};
