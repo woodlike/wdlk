a5f41d780bfeab718cdf95d6c2f1bc73db6b1e7d
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-03-10 20:19:41 +0100

    fix(components): remove lint errors

--- packages/components/src/utils/utils.ts
+++ packages/components/src/utils/utils.ts
@@ -3,8 +3,8 @@ import { qt } from '../query';
 export function calcYPosition(scales: string[], fontS: number, fontM: number): string[] {
   const values = scales.map((scale: string) =>
     scale.split(' ').reduce((acc, curr) => {
-      const char = curr.match(/[0-9]/g)?.join('');
-      return char ? (acc = parseInt(char)) : acc;
+      const char = curr.match(/[0-9]/g);
+      return char ? (acc = parseInt(char.join(''))) : acc;
     }, 0),
   );
   return values.map(val => (val <= Math.max(...values) ? `${val - fontS * 1.5 - 3}px` : `${val - fontM * 1.5 + 2}px`));
