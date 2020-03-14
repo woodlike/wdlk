acba68ac2a44fece5f4878376a1447e157a5e11c
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-03-14 21:25:31 +0100

    feat(components): extract AST creation nodes into a separate file

---
+++ packages/components/node/create-ast-nodes.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import {
+  ArgumentPlaceholder,
+  CallExpression,
+  LVal,
+  ExportNamedDeclaration,
+  MemberExpression,
+  ImportDeclaration,
+} from '@babel/types';
+const t = require('@babel/types');
+
+function exportConst(name: string, callee: CallExpression, args: ArgumentPlaceholder[]): ExportNamedDeclaration {
+  const id = {
+    type: 'Identifier',
+    name,
+  } as LVal;
+  return t.exportNamedDeclaration(
+    t.variableDeclaration('const', [t.variableDeclarator(id, t.callExpression(callee, [...args]))]),
+    [],
+  );
+}
+
+exports.exportConst = exportConst;
+
+function assignmentExp(name: string, init: MemberExpression): {} {
+  const id = {
+    type: 'Identifier',
+    name,
+  } as LVal;
+  return t.exportNamedDeclaration(t.variableDeclaration('const', [t.variableDeclarator(id, init)]), []);
+}
+
+exports.assignmentExp = assignmentExp;
+
+function importDeclaration(name: string, moduleName: string): ImportDeclaration {
+  return t.importDeclaration([t.importNamespaceSpecifier(t.identifier(name))], t.stringLiteral(moduleName));
+}
+
+exports.importDeclaration = importDeclaration;
--- packages/components/src/code/__prism-languages/prism-typescript.ts
+++ packages/components/src/code/__prism-languages/prism-typescript.ts
@@ -1,4 +1,5 @@
 /*This is an auto-generated file to override the default Prism languages. Check the node directory to see the implementation or run the prism:lang command on this package to generate a fresh set of languages.*/
+import * as Prism from 'prismjs';
 export const typescript = Prism.languages.extend('javascript', {
   // From JavaScript Prism keyword list and TypeScript language spec: https://github.com/Microsoft/TypeScript/blob/master/doc/spec.md#221-reserved-words
   keyword: /\b(?:abstract|as|async|await|break|case|catch|class|const|constructor|continue|debugger|declare|default|delete|do|else|enum|export|extends|finally|for|from|function|get|if|implements|import|in|instanceof|interface|is|keyof|let|module|namespace|new|null|of|package|private|protected|public|readonly|return|require|set|static|super|switch|this|throw|try|type|typeof|undefined|var|void|while|with|yield)\b/,
