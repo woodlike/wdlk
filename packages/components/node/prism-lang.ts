839c1e9f8dbd6c5f20c69f5879fb912f4f835616
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-03-14 21:25:31 +0100

    feat(components): extract AST creation nodes into a separate file

--- packages/components/node/prism-lang.ts
+++ packages/components/node/prism-lang.ts
@@ -1,19 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import {
-  ArgumentPlaceholder,
-  CallExpression,
-  ExpressionStatement,
-  LVal,
-  MemberExpression,
-  Statement,
-  Program,
-} from '@babel/types';
+import { ArgumentPlaceholder, CallExpression, File, MemberExpression, Statement, Program } from '@babel/types';
 const { default: generate } = require('@babel/generator');
 const { dirname, resolve } = require('path');
 const { parse } = require('@babel/parser');
 const { readdir, readFile, writeFile } = require('fs');
 const prettier = require('prettier');
-const t = require('@babel/types');
+const create = require('./create-ast-nodes');
 
 type GetExpressionType = [string, CallExpression, ArgumentPlaceholder[]] | [string, MemberExpression];
 const pathToPrism = dirname(require.resolve('prismjs'));
@@ -38,72 +30,59 @@ const languages = [
 const introFileComment =
   'This is an auto-generated file to override the default Prism languages. Check the node directory to see the implementation or run the prism:lang command on this package to generate a fresh set of languages.';
 
-function createExportCallExpNode(name: string, callee: CallExpression, args: ArgumentPlaceholder[]): {} {
-  const id = {
-    type: 'Identifier',
-    name,
-  } as LVal;
-  return t.exportNamedDeclaration(
-    t.variableDeclaration('const', [t.variableDeclarator(id, t.callExpression(callee, [...args]))]),
-    [],
-  );
-}
-
-function createAssignmentExpNode(name: string, init: MemberExpression): {} {
-  const id = {
-    type: 'Identifier',
-    name,
-  } as LVal;
-  return t.exportNamedDeclaration(t.variableDeclaration('const', [t.variableDeclarator(id, init)]), []);
-}
-
-function getExpression(node: ExpressionStatement): GetExpressionType {
-  const { expression } = node;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const { left, right } = (expression as unknown) as any;
-  const name = left.property.name;
-  return right.callee ? [name, right.callee, right.arguments] : [name, right.object];
-}
-
 function parseAst(path: string, file: string): void {
   readFile(`${path}/${file}`, 'utf-8', (err: Error, source: string) => {
     if (err) {
       throw Error(`There was an error reading the file: ${err}`);
     }
-    const prismAst = parse(`${source}`);
+    const ast = parse(`${source}`);
     const name = file.split('.');
     name.splice(1, 1, 'ts');
 
-    const body = prismAst.program.body.map((node: ExpressionStatement) => {
-      const nodes = getExpression(node);
-      if (nodes.length === 3) {
-        const [name, callee, args] = nodes;
-        return createExportCallExpNode(name, callee, args);
-      }
-      const [name, init] = nodes;
-      return createAssignmentExpNode(name, init);
-    });
-
-    generateCode(body, name.join('.'));
+    generateCode(convertASTBody(ast), name.join('.'));
   });
+}
+
+function convertASTBody(ast: Partial<File>): Statement[] | [] {
+  if (!ast || !ast.program) {
+    return [];
+  }
+  return ast.program.body.map(node => {
+    const nodes = getExpression(node);
+    if (nodes.length === 3) {
+      const [name, callee, args] = nodes;
+      return create.exportConst(name, callee, args);
+    }
+    const [name, init] = nodes;
+    return create.assignmentExp(name, init);
+  }) as Statement[];
+}
+
+function getExpression(node: Statement): GetExpressionType {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const { expression } = node as any;
+  const { left, right } = expression;
+  const name = left.property.name;
+  return right.callee ? [name, right.callee, right.arguments] : [name, right.object];
 }
 
 function generateCode(body: Statement[], file: string): void {
   const path = resolve('./src', 'code/__prism-languages/', file);
   const ast: Partial<Program> = {
     type: 'Program',
-    body: [...body],
+    body: [create.importDeclaration('Prism', 'prismjs'), ...body],
   };
   const generateConfig = {
     auxiliaryCommentBefore: introFileComment,
   };
   const prettierConfig = {
     bracketSpacing: true,
+    parser: 'babel',
     printWidth: 120,
-    trailingComma: 'all',
-    tabWidth: 2,
     semi: true,
     singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'all',
   };
   const { code } = generate(ast, generateConfig);
   console.log(code, '--------');
