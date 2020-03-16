b0e2baa296e71fdf36b9d45a3db895f382441da7
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-03-16 21:02:32 +0100

    feat(components): handle prism typescript

--- packages/components/node/prism-lang.test.ts
+++ packages/components/node/prism-lang.test.ts
@@ -1,5 +1,14 @@
-describe('handle AST body statement', () => {
-  it('should', () => {
-    expect('sanoe').toBe('sanoe');
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { typeScriptBodyAST } from './__mocks__';
+const { handleStatement } = require('./handle-statement');
+
+describe('handle AST body statements', () => {
+  it('should match the typescript language assignment array representation', () => {
+    expect(handleStatement(typeScriptBodyAST[0])[0]).toBe('typescript');
+    expect(handleStatement(typeScriptBodyAST[0])).toMatchSnapshot();
+  });
+  it('should match a ts to typescript language assignment representation', () => {
+    expect(handleStatement(typeScriptBodyAST[1])[0]).toBe('ts');
+    expect(handleStatement(typeScriptBodyAST[1])).toMatchSnapshot();
   });
 });
