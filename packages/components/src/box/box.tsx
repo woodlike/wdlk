e8c86c88698c9d05eb04a30e438d1fac4c8b722a
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-04-10 21:01:27 +0200

    feat(components): box border-color area implementation

--- packages/components/src/box/box.tsx
+++ packages/components/src/box/box.tsx
@@ -1,17 +1,18 @@
 /**@jsx jsx */
 import { jsx, SxStyleProp } from 'theme-ui';
-// import { ThemeQuery } from 'theme-query';
 
 import { ScaleArea, Theme } from '..';
-// import { useThemeQuery } from '../theme';
-import * as Scale from '../theme/scale';
+import { Color, Scale } from '../theme';
 
 export interface BoxProps {
   readonly padding: ScaleArea;
   readonly borderWidths?: ScaleArea;
   readonly borderStyles?: ScaleArea;
-  readonly borderColors?: ScaleArea;
+  readonly borderColors?: string | string[] | { color: string; idx: number };
 }
+
+export type BorderColorArea = string | string[] | BorderColorScale;
+export type BorderColorScale = { color: string; idx: number };
 
 const createStylesBorderWidth = (widths: ScaleArea): SxStyleProp => ({
   borderWidth: (theme: Theme) => Scale.toCSSPixel(Scale.create(widths, theme.borderWidths)),
@@ -21,28 +22,32 @@ const createStylesBorderStyle = (styles: ScaleArea): SxStyleProp => ({
   borderStyle: (theme: Theme) => Scale.toCSSPixel(Scale.create(styles, theme.borderStyles)),
 });
 
-// const createStylesBorderColor = (colorNames: any, qt: ThemeQuery): SxStyleProp => {
-//   const colors = Array.isArray(colorNames) ? colorNames.map(name => qt(`${name}`)) : qt(`${colorNames}`);
+const createStylesBorderColor = (borderColors: string | string[] | { color: string; idx: number }): SxStyleProp => {
+  return {
+    borderColor: (theme: Theme) => {
+      if (Array.isArray(borderColors)) {
+        const colors = borderColors.map(color => Color.query(color, theme.colors)) as string | string[];
+        return Scale.createBox(colors).join(' ');
+      }
+      if (typeof borderColors === 'object') {
+        const border = borderColors as BorderColorScale;
+        return Color.query(border.color, theme.colors, border.idx);
+      }
+      return Color.query(borderColors, theme.colors);
+    },
+  };
+};
 
-//   console.log(colors, '&&&&&&&&');
-//   return {
-//     borderColor: Scale.toCSSString(Scale.handleSpace(colors)),
-//   };
-// };
-
-const createStyles = (props: BoxProps /*qt: ThemeQuery*/): SxStyleProp => {
-  const { borderStyles, borderWidths, padding } = props;
+const createStyles = (props: BoxProps): SxStyleProp => {
+  const { borderColors, borderStyles, borderWidths, padding } = props;
   return {
     ...(borderWidths && createStylesBorderWidth(borderWidths)),
     ...(borderStyles && createStylesBorderStyle(borderStyles)),
-    // ...(borderColors && createStylesBorderColor(borderColors, qt)),
+    ...(borderColors && createStylesBorderColor(borderColors)),
     padding: ({ space }) => Scale.toCSSPixel(Scale.create(padding, space)),
   };
 };
 
-export const Box: React.FC<BoxProps> = (props): JSX.Element => {
-  // const { qt } = useThemeQuery();
-  return <div sx={createStyles(props)}>{props.children}</div>;
-};
+export const Box: React.FC<BoxProps> = (props): JSX.Element => <div sx={createStyles(props)}>{props.children}</div>;
 
 Box.displayName = 'Box';
