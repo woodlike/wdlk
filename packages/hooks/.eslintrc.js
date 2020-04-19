629bd3009f5e56d2edfa202cdf5dd3592454a8f8
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-04-19 21:04:26 +0200

    fix(hooks): remove nested rule property from eslintrc

--- packages/hooks/.eslintrc.js
+++ packages/hooks/.eslintrc.js
@@ -2,12 +2,10 @@
 module.exports = {
   ...require('../../.eslintrc.js'),
   rules: {
-    rules: {
-      'react/prop-types': 'off',
-      '@typescript-eslint/explicit-function-return-type': [
-        'warn',
-        { allowExpressions: true }
-      ],
-    }
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': [
+      'warn',
+      { allowExpressions: true }
+    ],
   }
 };
