b0f6066e7a665ca953334e71f9a2cd3fffc40035
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-04-23 22:57:48 +0200

    feat(docs): create basic Documents node implementation

--- packages/docs/gatsby-node.js
+++ packages/docs/gatsby-node.js
@@ -1,6 +1,28 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
+const { createCompiler } = require('@mdx-js/mdx');
 const { createFilePath } = require('gatsby-source-filesystem');
+const { basename } = require('path');
+const uuidv5 = require('uuid/v5');
+const vfile = require('vfile');
+
+const createDocsNode = init => ({
+  id: init.id,
+  baseName: init.baseName,
+  parent: init.parent,
+  children: [],
+  transcluded: [],
+  internal: {
+    type: 'Document',
+    content: init.content,
+    description: init.description,
+  },
+});
+
+const createDocTranscluded = init => ({
+  documentation: init.documentation,
+  display: init.display,
+});
 
 const sanitizeSlugPath = value => {
   if (typeof value !== 'string') {
@@ -8,8 +30,45 @@ const sanitizeSlugPath = value => {
   }
   const strings = value.split('/');
   const idx = strings.findIndex(str => str === 'content');
-  // Coparing idx with 1 because Gatsby interprets Boolean(-1) as truthy
+  // Comparing idx with 1 because Gatsby interprets Boolean(-1) as truthy
   return strings.splice(idx === 1 ? idx + 1 : 1, 1).join('/');
+};
+
+exports.sourceNodes = ({ getNodesByType }) => {
+  const nodes = getNodesByType('Mdx');
+  const mdxCompiler = createCompiler();
+  const docCollection = [];
+  const NAMESPACE = '1b671a64-40d5-491e-99b0-da01ff1f3341';
+  nodes.forEach(node => {
+    if (node.internal.type === 'Mdx') {
+      const mdxAst = mdxCompiler.parse(vfile(node.internal.content));
+      const displayNode = mdxAst.children.find(
+        ({ type, value }) => typeof value === 'string' && value.includes('displayCode') && type === 'export',
+      );
+      const initDocNode = {
+        ...createDocsNode({
+          id: uuidv5(basename(node.fileAbsolutePath), NAMESPACE),
+          baseName: basename(node.fileAbsolutePath),
+          parent: node.parent,
+          children: [],
+          internal: {
+            content: node.content,
+            description: node.description,
+          },
+        }),
+      };
+
+      initDocNode.transcluded.push(
+        createDocTranscluded({
+          documentation: node.content,
+          ...(displayNode && { display: displayNode }),
+        }),
+      );
+      //TODO: merge documents of the same category
+      // create Gatsby Node
+      docCollection.push(initDocNode);
+    }
+  });
 };
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
@@ -25,5 +84,26 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
       node,
       value: `/${path}`,
     });
+  }
+};
+
+exports.createPages = async ({ graphql, reporter }) => {
+  const result = await graphql(`
+    query {
+      allMdx {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+            mdxAST
+          }
+        }
+      }
+    }
+  `);
+  if (result.errors) {
+    reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query');
   }
 };
