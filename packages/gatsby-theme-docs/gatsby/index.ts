7a3925898f83a81ce21814abe875d9685ebbf292
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-05-25 21:51:56 +0200

    feat(gatsby-theme-docs): include MDXAst interface

--- packages/gatsby-theme-docs/gatsby/index.ts
+++ packages/gatsby-theme-docs/gatsby/index.ts
@@ -1,1 +1,5 @@
-
+export * from './create-node';
+export * from './create-pages';
+export * from './docs';
+export * from './mdx-ast';
+export * from './pre-bootstrap';
---
+++ packages/gatsby-theme-docs/gatsby/mdx-ast.ts
@@ -0,0 +1,52 @@
+import yaml from 'yaml';
+
+export interface MDXAst {
+  type: string;
+  children: MDXAstNode[];
+}
+
+export interface MDXAstNode {
+  type: string;
+  position: MDXAstNodePosition;
+  value?: string;
+  depth?: number;
+  children?: MDXAstNode[];
+}
+
+export interface MDXAstNodePosition {
+  start: Position;
+  end: Position;
+  indent: number[] | [];
+}
+
+export interface Position {
+  line: number;
+  column: number;
+  offset: number;
+}
+
+export interface Frontmatter {
+  name: string;
+  title: string;
+  menu: string;
+}
+
+export const getDisplay = (mdxAst: MDXAst): string => {
+  const node = mdxAst.children.find(
+    ({ type, value }) =>
+      typeof value === 'string' &&
+      value.includes('_display') &&
+      type === 'export',
+  );
+  return node && node.value ? node.value : '';
+};
+
+export const getFrontmatter = (mdxAst: MDXAst): Frontmatter => {
+  const node = mdxAst.children.find(node => node.type === 'yaml' && node.value);
+  return node && node.value
+    ? yaml.parse(node.value)
+    : { menu: '', name: '', title: '' };
+};
+
+export const findNodeByType = (mdxAst: MDXAst, type: string): MDXAstNode | {} =>
+  mdxAst.children.find(node => node.type === type && node.value) || {};
