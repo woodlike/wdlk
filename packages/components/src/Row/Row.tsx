b180b71784d6d391851738b540cbee4c1687efc3
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-06-03 20:39:00 +0200

    feat(components): Row component make as property optional

--- packages/components/src/Row/Row.tsx
+++ packages/components/src/Row/Row.tsx
@@ -2,26 +2,42 @@
 import { jsx, SxStyleProp } from 'theme-ui';
 
 export interface RowProps {
-  as: HTMLRowType;
+  as?: HTMLRowType;
   basis?: RowFlexBasis;
   className?: string;
 }
-export type RowFlexBasis = 'fluid' | '1/2' | '1/3' | '2/3' | '1/4' | '3/4' | '1/5' | '2/5' | '3/5' | '4/5';
+export type RowFlexBasis =
+  | 'fluid'
+  | '1/2'
+  | '1/3'
+  | '2/3'
+  | '1/4'
+  | '3/4'
+  | '1/5'
+  | '2/5'
+  | '3/5'
+  | '4/5';
 
 export type HTMLRowType = 'div' | 'section' | 'aside' | 'article';
 
 const calculateFlexBasis = (basis: RowFlexBasis): string =>
-  basis.split('/').reduce((prev: string, curr: string) => ((+prev * 100) / +curr).toString());
+  basis
+    .split('/')
+    .reduce((prev: string, curr: string) => ((+prev * 100) / +curr).toString());
 
 const createStylesRow = (basis: RowFlexBasis | undefined): SxStyleProp => ({
-  ...(basis && basis !== 'fluid' && { flex: `0 0 ${calculateFlexBasis(basis)}%` }),
+  ...(basis &&
+    basis !== 'fluid' && { flex: `0 0 ${calculateFlexBasis(basis)}%` }),
   boxSizing: 'border-box',
 });
 
-export const Row: React.FC<RowProps> = props => (
-  <props.as sx={createStylesRow(props.basis)} className={props.className}>
-    {props.children}
-  </props.as>
-);
+export const Row: React.FC<RowProps> = props => {
+  const Component = props.as || 'div';
+  return (
+    <Component sx={createStylesRow(props.basis)} className={props.className}>
+      {props.children}
+    </Component>
+  );
+};
 
 Row.displayName = 'Row';
