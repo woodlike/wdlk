c7cccbbd682b37278a57f56fa5753568a1e2ec10
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-06-03 15:38:45 +0200

    feat(@wdlk/hooks): implement intersection hook

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,5 +1,6 @@
 import * as IntIntersection from './intersection';
 
+export * from './use-intersection-observer';
 export * from './use-video-control';
 
 export const Intersection = { ...IntIntersection };
