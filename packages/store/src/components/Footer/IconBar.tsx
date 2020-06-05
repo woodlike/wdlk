ed982e77db4961e6fd1ef5f264cb85dac53252d4
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-06-05 20:53:23 +0200

    feat(store): implement footer icons

---
+++ packages/store/src/components/Footer/IconBar.tsx
@@ -0,0 +1,44 @@
+/**@jsx jsx */
+import { jsx, SxStyleProp } from 'theme-ui';
+import { Box, Rows } from '@wdlk/components';
+
+import { Icon, IconSize } from '..';
+
+const stylesIconBar: SxStyleProp = {
+  paddingBottom: 7,
+};
+
+const stylesIcon: SxStyleProp = {
+  alignSelf: 'center',
+};
+
+export const IconBar: React.FC = () => (
+  <Rows sx={stylesIconBar} justifyContent="center">
+    <Box sx={stylesIcon} padding={[0, 5]}>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://directories.onepercentfortheplanet.org/business-members/woodlike">
+        <Icon.OnePercent size={IconSize.s} />
+      </a>
+    </Box>
+    <Box sx={stylesIcon} padding={[0, 5]}>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.instagram.com/wdlk/">
+        <Icon.Instagram size={IconSize.s} />
+      </a>
+    </Box>
+    <Box sx={stylesIcon} padding={[0, 5]}>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.facebook.com/woodlike">
+        <Icon.Facebook size={IconSize.s} />
+      </a>
+    </Box>
+  </Rows>
+);
+
+IconBar.displayName = 'IconBar';
---
+++ packages/store/src/components/Icon/index.ts
@@ -0,0 +1,7 @@
+export * from './sizes';
+
+import * as FacebookIcon from './Facebook';
+import * as InstagramIcon from './Instagram';
+import * as OnePercentIcon from './OnePercent';
+
+export const Icon = { ...FacebookIcon, ...InstagramIcon, ...OnePercentIcon };
