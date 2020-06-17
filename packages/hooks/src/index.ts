edecaa4e2f08b988abebf8a2dfaadf6bf38b6a33
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-06-17 10:48:49 +0200

    feat(hooks): upgrade the Typescript version

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,7 +1,3 @@
-import * as IntIntersection from './intersection';
-
 export * from './use-intersection-observer';
-export * from './use-video-control';
 export * from './use-media';
-
-export const Intersection = { ...IntIntersection };
+export * from './use-video-control';
