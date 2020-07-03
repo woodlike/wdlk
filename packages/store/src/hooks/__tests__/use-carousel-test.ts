d747373f7e554c2f7eaed5b86a83ce0dff58995e
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-07-03 17:41:47 +0200

    feat(store): implement use carousel hook

--- packages/store/src/hooks/__tests__/use-carousel-test.ts
+++ packages/store/src/hooks/__tests__/use-carousel-test.ts
@@ -1,6 +1,40 @@
-import { previous, next } from '..';
+import { renderHook, act } from '@testing-library/react-hooks';
+import { previous, next, useCarousel } from '..';
 
-describe('useCarousel()', () => {
+describe('UseCarousel', () => {
+  describe('useCarousel()', () => {
+    it('should return a default coordinate of 0', () => {
+      const { result } = renderHook(() => useCarousel(4));
+      const { coordinate } = result.current;
+      expect(coordinate).toBe(0);
+    });
+    it('should return the previous coordinate if the passed argument is larger than the amount of items', () => {
+      const { result } = renderHook(() => useCarousel(4));
+      act(() => {
+        result.current.set(2);
+      });
+      expect(result.current.coordinate).toBe(200);
+
+      act(() => {
+        result.current.set(5);
+      });
+
+      expect(result.current.coordinate).toBe(200);
+    });
+
+    it.each([0, 1, 2, 3, 4, 5, 6, 7, 8])(
+      'should return the i% coordinate',
+      current => {
+        const { result } = renderHook(() => useCarousel(9));
+
+        act(() => {
+          result.current.set(current);
+        });
+
+        expect(result.current.coordinate).toBe(current * 100);
+      },
+    );
+  });
   describe('previous()', () => {
     it.each([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0])(
       'should return the previous index of %i',
--- packages/store/src/hooks/use-carousel.ts
+++ packages/store/src/hooks/use-carousel.ts
@@ -1,7 +1,30 @@
+import { useState, useCallback, useMemo } from 'react';
+
+export interface UseCarousel {
+  readonly coordinate: number;
+  set(curr: number): void;
+}
+
 export function previous(current: number, length: number): number {
   return (current - 1 + length) % length;
 }
 
 export function next(current: number, length: number): number {
   return (current + 1 + length) % length;
+}
+
+export function useCarousel(length: number): UseCarousel {
+  const [coordinate, setCoordinate] = useState<number>(0);
+  const [current, setCurrent] = useState<number>(0);
+
+  const set = useCallback((n: number) => setCurrent(n), [current]);
+  useMemo(
+    () =>
+      setCoordinate(coordinate =>
+        current > length ? coordinate : current * 100,
+      ),
+    [current, coordinate],
+  );
+
+  return { coordinate, set };
 }
