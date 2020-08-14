42e3ce329ec7ad1604b1d12d59282fd774ead5b7
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-08-14 15:16:31 +0200

    feat(store): change price formatter formating template

--- packages/store/src/utils/price-formatter.ts
+++ packages/store/src/utils/price-formatter.ts
@@ -3,14 +3,8 @@ export function priceFormatter(
   locale: string,
   currency: string,
 ): string {
-  const value = parseFloat(srcPrice)
-    .toLocaleString(locale, {
-      style: 'currency',
-      currency: currency,
-    })
-    .split('');
-
-  const sign = value.splice(0, 1);
-
-  return [...value, ' ', ...sign].join('');
+  return parseFloat(srcPrice).toLocaleString(locale, {
+    style: 'currency',
+    currency: currency,
+  });
 }
