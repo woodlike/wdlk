e3a371996cc2582949a06d00f1911b826a9512b9
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-08-17 22:42:41 +0200

    feat(components): include hover styles button

--- packages/components/src/Button.tsx
+++ packages/components/src/Button.tsx
@@ -6,6 +6,7 @@ import { Theme, Scale, ScaleArea } from '.';
 import { getVariant } from './theme';
 
 export interface ButtonProps {
+  readonly onclick: React.MouseEventHandler<HTMLButtonElement>;
   readonly padding?: ScaleArea;
   readonly variant?: ButtonVariant;
 }
@@ -18,21 +19,62 @@ interface StyledButtonProps {
 
 type ButtonVariant = 'primary' | 'secondary';
 
+const pseudoElementStyles = css`
+  content: '';
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+`;
+
 const StyledButton = styled.button<StyledButtonProps>`
+  position: relative;
+  width: 100%;
   padding: ${({ padding, theme }) =>
     Scale.toCSSPixel(Scale.create(padding || 1, theme.space))};
+  border: none;
+  letter-spacing: 1px;
+  line-height: 1;
   text-align: center;
   text-transform: uppercase;
-
+  cursor: pointer;
+  overflow: hidden;
+  background: transparent;
   ${({ variant, theme }) => {
     const result = getVariant(theme, 'buttons', variant || 'primary');
     return result
       ? css`
           color: ${result.color};
-          background-color: ${result.bg};
+          ::before {
+            background-color: ${result.bg};
+          }
+          ::after {
+            background-color: ${result.bg};
+          }
         `
       : css``;
-  }}
+  }};
+
+  ::before {
+    ${pseudoElementStyles}
+  }
+
+  ::after {
+    ${pseudoElementStyles}
+    filter: contrast(0.7);
+    pointer-events: none;
+    transform: translate3d(0, 100%, 0);
+    transition: transform 100ms linear;
+  }
+
+  :hover {
+    &::after {
+      transform: translate3d(0, 0, 0);
+      pointer-events: auto;
+    }
+  }
 `;
 
 StyledButton.displayName = 'StyledButton';
