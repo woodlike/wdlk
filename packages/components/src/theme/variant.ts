5452f47ee4b1f4495093b3007b8d7ae1604b2988
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-08-17 21:16:20 +0200

    feat(components): implement button component variants

---
+++ packages/components/src/theme/variant.ts
@@ -0,0 +1,34 @@
+import { Theme } from '.';
+
+export interface ButtonVariant {
+  readonly color: string;
+  readonly bg: string;
+}
+
+export interface ButtonVariants {
+  readonly primary: ButtonVariant;
+  readonly secondary: ButtonVariant;
+}
+
+export type Variant = ButtonVariant;
+export type Variants = ButtonVariants;
+
+export type ThemeVariants = Pick<Theme, 'buttons'>;
+
+export function getKeyValue<T, K extends keyof T>(obj: T, key: K): T[K] | null {
+  if (!(key in obj)) {
+    return null;
+  }
+  return obj[key];
+}
+
+export function getVariant(
+  theme: ThemeVariants,
+  key: keyof ThemeVariants,
+  value: keyof Variants,
+): Variant | null {
+  const variants = getKeyValue<ThemeVariants, keyof ThemeVariants>(theme, key);
+  return !!variants
+    ? getKeyValue<Variants, keyof Variants>(variants, value)
+    : null;
+}
