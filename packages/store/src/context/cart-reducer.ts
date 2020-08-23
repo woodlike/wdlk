ed544e795da32cefd7f84fe0301d3fd249bc11fd
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-08-23 16:11:13 +0200

    feat(store): abstract shopify client initialization

--- packages/store/src/context/cart-reducer.ts
+++ packages/store/src/context/cart-reducer.ts
@@ -1,3 +1,5 @@
+import { Dispatch } from 'react';
+
 export interface CartState {
   readonly client: ShopifyBuy.Client;
   readonly cart: ShopifyBuy.Cart;
@@ -8,11 +10,35 @@ export interface ActionType<T extends string, P = undefined> {
   readonly payload: P;
 }
 
-export type Action = ActionType<'update_checkout', ShopifyBuy.Cart>;
+export type Action = ActionType<'initialize_checkout', ShopifyBuy.Cart>;
+
+export async function initializeCheckout(
+  client: ShopifyBuy.Client,
+  id: string | null,
+  dispatch: Dispatch<Action>,
+) {
+  try {
+    if (!!id) {
+      const cart = await client.checkout.fetch(id);
+      if (!cart || !cart.id) {
+        const cart = await client.checkout.create();
+        localStorage.setItem('shopify_checkout_id', `${cart.id}`);
+        dispatch({ type: 'initialize_checkout', payload: cart });
+      }
+    } else {
+      const cart = await client.checkout.create();
+      localStorage.setItem('shopify_checkout_id', `${cart.id}`);
+      dispatch({ type: 'initialize_checkout', payload: cart });
+    }
+  } catch (error) {
+    console.warn(`CartProvider Shopify cart init reducer: ${error}`);
+    localStorage.setItem('shopify_checkout_id', '');
+  }
+}
 
 export function cartReducer(state: CartState, action: Action) {
   switch (action.type) {
-    case 'update_checkout': {
+    case 'initialize_checkout': {
       return {
         ...state,
         cart: action.payload,
