ab88ed30ff02f071272d17219ec3be057fe208d0
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-09-07 15:28:54 +0200

    refactor(components): replaced theme-ui in the cart component

--- packages/components/src/__tests__/Cart.test.tsx
+++ packages/components/src/__tests__/Cart.test.tsx
@@ -1,9 +1,10 @@
+import * as React from 'react';
+import { ThemeProvider } from 'emotion-theming';
 import { axe, toHaveNoViolations } from 'jest-axe';
 import { matchers } from 'jest-emotion';
 import { render, cleanup } from '@testing-library/react';
-import * as React from 'react';
 
-import { Cart } from '.';
+import { Cart, theme } from '..';
 
 expect.extend(matchers);
 expect.extend(toHaveNoViolations);
@@ -11,7 +12,15 @@ expect.extend(toHaveNoViolations);
 describe('<MiniCart />', () => {
   it('should not have accessibility violations', async done => {
     const { container, unmount } = render(
-      <Cart href="#" isFocused={false} count={0} isFilled={true} title="Woodlike Ocean Shopping cart" />,
+      <ThemeProvider theme={theme}>
+        <Cart
+          href="#"
+          isFocused={false}
+          count={0}
+          isFilled={true}
+          title="Woodlike Ocean Shopping cart"
+        />
+      </ThemeProvider>,
     );
 
     const a11yResults = await axe(container);
@@ -25,35 +34,64 @@ describe('<MiniCart />', () => {
     const count = 0;
     const result = 'Woodlike Ocean Shopping cart';
     const { getByTestId, unmount } = render(
-      <Cart href="#" isFocused={false} count={count} isFilled={false} title={result} />,
+      <ThemeProvider theme={theme}>
+        <Cart
+          href="#"
+          isFocused={false}
+          count={count}
+          isFilled={false}
+          title={result}
+        />
+      </ThemeProvider>,
     );
     const cart = getByTestId('cart-link-test-id');
     const title = cart.querySelector('title') as HTMLTitleElement;
     const desc = cart.querySelector('desc') as SVGDescElement;
 
     expect(title.innerHTML).toBe(result);
-    expect(desc.innerHTML).toBe(`The cart currently contains ${count} items`);
+    expect(desc.innerHTML).toBe(
+      `The shopping bag currently contains ${count} items`,
+    );
     unmount();
   });
 
   it('should be filled and should contain 2 items', () => {
     const result = 2;
     const { getByTestId, unmount } = render(
-      <Cart href="#" isFocused={false} count={result} isFilled={true} title="Woodlike Ocean Shopping cart" />,
+      <ThemeProvider theme={theme}>
+        <Cart
+          href="#"
+          isFocused={false}
+          count={result}
+          isFilled={true}
+          title="Woodlike Ocean Shopping cart"
+        />
+      </ThemeProvider>,
     );
 
     const cart = getByTestId('cart-link-test-id');
     const count = cart.querySelector('text') as SVGTextElement;
     const desc = cart.querySelector('desc') as SVGDescElement;
+
     expect(count.innerHTML).toBe(result.toString(10));
-    expect(desc.innerHTML).toBe(`The cart currently contains ${result} items`);
+    expect(desc.innerHTML).toBe(
+      `The shopping bag currently contains ${count.innerHTML} items`,
+    );
     expect(cart).toHaveStyleRule('fill-opacity', '1');
     unmount();
   });
 
   it('should not be filled and should contain 0 items', () => {
     const { getByTestId, unmount } = render(
-      <Cart href="#" isFocused={false} count={0} isFilled={false} title="Woodlike Ocean Shopping cart" />,
+      <ThemeProvider theme={theme}>
+        <Cart
+          href="#"
+          isFocused={false}
+          count={0}
+          isFilled={false}
+          title="Woodlike Ocean Shopping cart"
+        />
+      </ThemeProvider>,
     );
 
     const cart = getByTestId('cart-link-test-id');
