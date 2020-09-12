905c641fbbdc17244e4831f07f7513f2da1e866b
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-09-12 18:35:05 +0200

    refactor(components): introduce themeable fonts for the heading component

--- packages/components/src/Heading.tsx
+++ packages/components/src/Heading.tsx
@@ -3,22 +3,27 @@ import styled from './styled';
 
 export interface HeadingProps {
   readonly size: HeadlineSize;
+  readonly type: HeadingFamily;
   readonly as?: HeadingLevel;
   readonly isInverted?: boolean;
 }
 
-export type HeadingFamily = 'display' | 'secondary' | 'campaign';
+export type HeadingFamily = 'primary' | 'secondary' | 'campaign';
 export type HeadingLevel = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'strong';
 export type HeadlineSize = 'xs' | 's' | 'm' | 'l' | 'xl' | 'xxl';
 
 const StyledHeading = styled.h1<HeadingProps>`
   margin: 0;
-  font-family: ${props => props.theme.heading.fontFamily};
   font-kerning: normal;
+  font-family: ${props => {
+    const { type, theme } = props;
+    return !!theme.heading.fonts[type] ? theme.heading.fonts[type] : '';
+  }};
   font-size: ${props => {
     const { size, theme } = props;
     return !!theme.heading[size] ? `${theme.heading[size].fontSize}px` : '';
   }};
+  font-weight: ${props => props.theme.heading.fontWeight};
   color: ${props => {
     const { isInverted, theme } = props;
     return isInverted ? theme.heading.modes.color : theme.heading.color;
@@ -33,7 +38,8 @@ export const Heading: React.FC<HeadingProps> = props => (
   <StyledHeading
     as={props.as || 'h1'}
     isInverted={!!props.isInverted}
-    size={props.size}>
+    size={props.size}
+    type={props.type}>
     {props.children}
   </StyledHeading>
 );
