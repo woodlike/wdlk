8e0cb6a6a42d84e4e6507e85dd99357d4671f115
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-09-17 16:35:24 +0200

    feat(store): remove the copyright component

--- packages/store/src/hooks/index.ts
+++ packages/store/src/hooks/index.ts
@@ -2,5 +2,6 @@ export * from './use-footer-links';
 export * from './use-header-data';
 export * from './use-navigation-data';
 export * from './use-product-data';
+export * from './use-site-data';
 export * from './use-social-links';
 export * from './types';
