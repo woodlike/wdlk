9f7d9ee0611297e5a6a2de7ec61ea836eb20dd46
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-10-11 19:52:45 +0200

    feat(store): include unittest for Product Features resolver

---
+++ packages/store/__mocks__/graphcms-product.ts
@@ -0,0 +1,173 @@
+export const graphcmsProducts = [
+  {
+    remoteTypeName: 'Product',
+    remoteId: 'ckg2cpheo08ey0150xpne15df',
+    locale: 'en',
+    stage: 'PUBLISHED',
+    createdAt: '2020-10-09T14:34:36.491668+00:00',
+    updatedAt: '2020-10-09T15:07:50.045468+00:00',
+    publishedAt: '2020-10-11T12:13:27.546932+00:00',
+    title: 'ALLI One Piece',
+    modelTitle: 'One Piece',
+    modelDescription:
+      '* Seamless finish.\n' +
+      '* Scoop neckline, flattering any bust cup.\n' +
+      '* Internal bustier with removable soft cups.\n' +
+      '* Double-lined for more support.\n' +
+      '* Slight shape effect.',
+    fitAndCoverageTitle: 'Fit & Coverage Guide',
+    fitAndCoverageDescription:
+      '* Moderate coverage.\n' +
+      '* Lovely for all body shapes.\n' +
+      '* Model is wearing the size small.\n' +
+      '* If you fall between sizes, we suggest purchasing the size up as our fit is graded slightly smaller.\n' +
+      '* Model’s measurements: Height 170 cm, Bust 86 cm, Waist 69 cm, Hips 94 cm, Dress size 34/36',
+    fabricFeature: {
+      remoteTypeName: 'FabricFeature',
+      remoteId: 'ckg2c8yi0086i0108g6bpgk0d',
+      locale: 'en',
+    },
+    productMarineProtection: {
+      remoteTypeName: 'ProductMarineProtection',
+      remoteId: 'ckg2c4yzc083d0108xqhpg0nj',
+      locale: 'en',
+    },
+    id: 'Product:ckg2cpheo08ey0150xpne15df:en',
+    parent: null,
+    internal: {
+      contentDigest: '6ac6dddaf5ca492f131f71be117a08df',
+      type: 'GraphCMS_Product',
+      counter: 29,
+      owner: 'gatsby-source-graphcms',
+    },
+    children: [],
+  },
+  {
+    remoteTypeName: 'Product',
+    remoteId: 'ckg2e5f0809bs01086ngbgiun',
+    locale: 'en',
+    stage: 'PUBLISHED',
+    createdAt: '2020-10-09T15:14:59.431347+00:00',
+    updatedAt: '2020-10-09T15:14:59.431347+00:00',
+    publishedAt: '2020-10-09T15:16:58.515117+00:00',
+    title: 'ANAIS Top',
+    modelTitle: 'Top',
+    modelDescription:
+      '* Reversible style.\n' +
+      '* Seamless finish.\n' +
+      '* Slightly lower scoop neckline.\n' +
+      '* Bust seam for a perfect fit.\n' +
+      '* Elastic underbust band for maximum support.',
+    fitAndCoverageTitle: 'Fit & Coverage Guide',
+    fitAndCoverageDescription:
+      '* Moderate coverage.\n' +
+      '* The top is flattering on both small and large busts.\n' +
+      '* For a comfy feeling choose one size bigger.\n' +
+      '* Model is wearing the size small.\n' +
+      '* Model’s measurements: Height 170 cm, Bust 86 cm, Waist 69 cm, Hips 94 cm, Dress size 34/36\n',
+    fabricFeature: {
+      remoteTypeName: 'FabricFeature',
+      remoteId: 'ckg2c8yi0086i0108g6bpgk0d',
+      locale: 'en',
+    },
+    productMarineProtection: {
+      remoteTypeName: 'ProductMarineProtection',
+      remoteId: 'ckg2c4yzc083d0108xqhpg0nj',
+      locale: 'en',
+    },
+    id: 'Product:ckg2e5f0809bs01086ngbgiun:en',
+    parent: null,
+    internal: {
+      contentDigest: 'fe79f6b2ab8bff5e27591374f3f493a1',
+      type: 'GraphCMS_Product',
+      counter: 30,
+      owner: 'gatsby-source-graphcms',
+    },
+    children: [],
+  },
+];
+
+export const graphcmsFabric = [
+  {
+    remoteTypeName: 'FabricFeature',
+    remoteId: 'ckg2c8yi0086i0108g6bpgk0d',
+    locale: 'en',
+    stage: 'PUBLISHED',
+    createdAt: '2020-10-09T14:21:45.009544+00:00',
+    updatedAt: '2020-10-09T15:37:38.977238+00:00',
+    publishedAt: '2020-10-09T15:37:41.680711+00:00',
+    internalTitle: 'Carvico Vita 390',
+    compositionTitle: 'Composition',
+    compositionDescription: '* 78% Nylon Econyl®\n* 22% Elastane',
+    title: 'Fabric Features',
+    fabricFeaturesDescription:
+      '* Shape retention\n' +
+      '* Excellent coverage (absolutely non-see-through)\n' +
+      '* UPF 50+ UV protection factor\n' +
+      '* Suntan creams & oils resistant\n' +
+      '* Ultra chlorine resistant\n' +
+      '* Soft and smooth\n' +
+      '* Made in Italy',
+    products: [
+      {
+        remoteTypeName: 'Product',
+        remoteId: 'ckg2cpheo08ey0150xpne15df',
+        locale: 'en',
+      },
+      {
+        remoteTypeName: 'Product',
+        remoteId: 'ckg2e5f0809bs01086ngbgiun',
+        locale: 'en',
+      },
+    ],
+    id: 'FabricFeature:ckg2c8yi0086i0108g6bpgk0d:en',
+    parent: null,
+    internal: {
+      contentDigest: '3aa6356e4b188ac2e0680f5e509a605e',
+      type: 'GraphCMS_FabricFeature',
+      counter: 28,
+      owner: 'gatsby-source-graphcms',
+    },
+    children: [],
+  },
+];
+
+export const graphcmsMarineProtection = [
+  {
+    remoteTypeName: 'ProductMarineProtection',
+    remoteId: 'ckg2c4yzc083d0108xqhpg0nj',
+    locale: 'en',
+    stage: 'PUBLISHED',
+    createdAt: '2020-10-09T14:18:39.052694+00:00',
+    updatedAt: '2020-10-09T14:18:39.052694+00:00',
+    publishedAt: '2020-10-09T14:18:53.694103+00:00',
+    title: 'Marine Protection',
+    description:
+      '* 1% for the Planet® donation.\n' +
+      '* The donation goes to the environmental organization Healthy Seas®.\n' +
+      '* Healthy Seas® removes ghost fishing nets from our oceans. \n' +
+      '* The same fishing nets used to create part of the ECONYL® yarn.\n' +
+      '* Sustainable - regenerative product.',
+    products: [
+      {
+        remoteTypeName: 'Product',
+        remoteId: 'ckg2cpheo08ey0150xpne15df',
+        locale: 'en',
+      },
+      {
+        remoteTypeName: 'Product',
+        remoteId: 'ckg2e5f0809bs01086ngbgiun',
+        locale: 'en',
+      },
+    ],
+    id: 'ProductMarineProtection:ckg2c4yzc083d0108xqhpg0nj:en',
+    parent: null,
+    internal: {
+      contentDigest: 'a0d68afc97a652ef6000ba2ab304521c',
+      type: 'GraphCMS_ProductMarineProtection',
+      counter: 27,
+      owner: 'gatsby-source-graphcms',
+    },
+    children: [],
+  },
+];
