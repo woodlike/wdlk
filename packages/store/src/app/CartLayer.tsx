187af08d1aafcb5f1ff6d1ea6bb56ea01e41d6c6
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-11-13 20:59:35 +0100

    feat(store): implement store cart summary

--- packages/store/src/app/CartLayer.tsx
+++ packages/store/src/app/CartLayer.tsx
@@ -1,9 +1,17 @@
-import React, { Dispatch, SetStateAction } from 'react';
-import { LayerAside, LayerShim, Columns, Heading } from '@wdlk/components';
+import React, { Dispatch, SetStateAction, useContext } from 'react';
+import {
+  LayerAside,
+  LayerShim,
+  Columns,
+  Heading,
+  Text,
+} from '@wdlk/components';
 import { useMedia } from '@wdlk/hooks';
 import { useTheme } from 'emotion-theming';
-import { Icon, IconSize } from '..';
 import { useStaticQuery, graphql } from 'gatsby';
+
+import { Icon, IconSize, Summary, Label, CartContext } from '..';
+import { SummaryItem } from '../components';
 
 export interface CartLayer {
   readonly isOpen: boolean;
@@ -19,11 +27,14 @@ export const CartLayer: React.FC<CartLayer> = props => {
         totalLabel
         shippingLabel
         itemsLabel
+        subtotalLabel
+        shippingPrecalculated
       }
     }
   `);
+  const { cart } = useContext(CartContext);
   const { breakpoints } = useTheme();
-  const isVisible = useMedia(
+  const isLargeViewPort = useMedia(
     [`(min-width: ${breakpoints[2]})`, `(min-width: ${breakpoints[1]})`],
     [true, false],
     false,
@@ -32,11 +43,11 @@ export const CartLayer: React.FC<CartLayer> = props => {
 
   return (
     <>
-      <LayerAside padding={[6, 8]} isOpen={isOpen}>
+      <LayerAside padding={[6, isLargeViewPort ? 8 : 0]} isOpen={isOpen}>
         <Columns
           align="center"
           justifyContent="space-between"
-          padding={[0, 0, 8, 0]}>
+          padding={isLargeViewPort ? [0, 0, 4, 0] : [0, 4, 4, 4]}>
           <Heading type="secondary" size="xs">
             {graphCmsCart.title}
           </Heading>
@@ -47,8 +58,45 @@ export const CartLayer: React.FC<CartLayer> = props => {
             color="secondary"
           />
         </Columns>
+        {cart ? (
+          <Summary>
+            <SummaryItem>
+              <Label size="s">
+                {cart.lineItems.length} {graphCmsCart.itemsLabel}
+              </Label>
+            </SummaryItem>
+            <SummaryItem justify="end">
+              {cart.subtotalPriceV2 && (
+                <Label size="s">
+                  {cart.subtotalPriceV2.amount}{' '}
+                  {cart.subtotalPriceV2.currencyCode}
+                </Label>
+              )}
+            </SummaryItem>
+            <SummaryItem>
+              <Label size="s">{graphCmsCart.shippingLabel}</Label>
+            </SummaryItem>
+            <SummaryItem justify="end">
+              <Label size="s">{graphCmsCart.shippingPrecalculated}</Label>
+            </SummaryItem>
+            <SummaryItem>
+              <Text as="div" size="s">
+                {graphCmsCart.totalLabel.toUpperCase()}
+              </Text>
+              <Label size="s">{graphCmsCart.vatValueLabel}</Label>
+            </SummaryItem>
+            <SummaryItem justify="end">
+              {cart.subtotalPriceV2 && (
+                <Heading type="secondary" size="m">
+                  {cart.subtotalPriceV2.amount}{' '}
+                  {cart.subtotalPriceV2.currencyCode}
+                </Heading>
+              )}
+            </SummaryItem>
+          </Summary>
+        ) : null}
       </LayerAside>
-      {isVisible && (
+      {isLargeViewPort && (
         <LayerShim isOpen={isOpen} onClick={() => setIsOpen(false)} />
       )}
     </>
