38cee9a70b1d0157d49e5483006ba29a6764d43f
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-11-13 19:11:20 +0100

    feat(store): include cart labels from graphcms

--- packages/store/src/app/CartLayer.tsx
+++ packages/store/src/app/CartLayer.tsx
@@ -1,7 +1,9 @@
 import React, { Dispatch, SetStateAction } from 'react';
-import { LayerAside, LayerShim } from '@wdlk/components';
+import { LayerAside, LayerShim, Columns, Heading } from '@wdlk/components';
 import { useMedia } from '@wdlk/hooks';
 import { useTheme } from 'emotion-theming';
+import { Icon, IconSize } from '..';
+import { useStaticQuery, graphql } from 'gatsby';
 
 export interface CartLayer {
   readonly isOpen: boolean;
@@ -9,6 +11,17 @@ export interface CartLayer {
 }
 
 export const CartLayer: React.FC<CartLayer> = props => {
+  const { graphCmsCart } = useStaticQuery(graphql`
+    query CartQuery {
+      graphCmsCart {
+        title
+        vatValueLabel
+        totalLabel
+        shippingLabel
+        itemsLabel
+      }
+    }
+  `);
   const { breakpoints } = useTheme();
   const isVisible = useMedia(
     [`(min-width: ${breakpoints[2]})`, `(min-width: ${breakpoints[1]})`],
@@ -16,10 +29,24 @@ export const CartLayer: React.FC<CartLayer> = props => {
     false,
   );
   const { isOpen, setIsOpen } = props;
+
   return (
     <>
       <LayerAside padding={[6, 8]} isOpen={isOpen}>
-        Hi Mom
+        <Columns
+          align="center"
+          justifyContent="space-between"
+          padding={[0, 0, 8, 0]}>
+          <Heading type="secondary" size="xs">
+            {graphCmsCart.title}
+          </Heading>
+          <Icon
+            onClick={() => setIsOpen(!isOpen)}
+            name="close"
+            size={IconSize.xs}
+            color="secondary"
+          />
+        </Columns>
       </LayerAside>
       {isVisible && (
         <LayerShim isOpen={isOpen} onClick={() => setIsOpen(false)} />
