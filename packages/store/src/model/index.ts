a952d08913a33318de9afe7d87b4f59a74da0957
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-12-05 17:31:06 +0100

    feat(store): replaced price formatter with currency

--- packages/store/src/model/index.ts
+++ packages/store/src/model/index.ts
@@ -1,2 +1,3 @@
 export * from './cart';
+export * from './currency';
 export * from './types';
--- packages/store/src/utils/index.ts
+++ packages/store/src/utils/index.ts
@@ -1,4 +1,3 @@
 export * from './generate-id';
-export * from './price-formatter';
 export * from './find-string';
 export * from './scales';
