3b2c24b7052fa8818421b0baee4c1e28730ecfab
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2021-01-29 20:51:28 +0100

    feat(store): implement legal page filter

---
+++ packages/store/src/model/page/__tests__/resolvers.test.ts
@@ -0,0 +1,60 @@
+import { ShopifyPage } from '../..';
+import { filterPageByTitle } from '../../utils';
+import { legalIds } from '..';
+import { shopifyPages } from '../../../../__mocks__';
+
+type LegalPageResult = Pick<ShopifyPage, 'slug'| 'title'>;
+
+describe('Page Resolvers', () => {
+  let legalPageResult: LegalPageResult[];
+
+  beforeEach(() => {
+    legalPageResult = [
+      {
+        title: 'Standard Business Terms and customer information',
+        slug: '/legal/terms-conditions'
+      },
+      {
+        title: 'Data protection declaration',
+        slug: '/legal/privacy-policy'
+      },
+      {
+        title: 'Allgemeine Geschäftsbedingungen und Kundeninformationen',
+        slug: '/legal/agb'
+      },
+      {
+        title: 'Impressum',
+        slug: '/legal/impressum'
+      },
+      {
+        title: 'Datenschutzerklärung',
+        slug: '/legal/datenschutzerklarung'
+      },
+      {
+        title: 'Returns & Exchanges',
+        slug: '/legal/cancellation-right'
+      },
+      {
+        title: 'About Us - Imprint',
+        slug: '/legal/imprint'
+      },
+      {
+        title: 'Widerrufsbelehrung',
+        slug: '/legal/widerrufsbelehrung'
+      },
+    ];
+  })
+
+  afterEach(() => {
+    legalPageResult = undefined as unknown as LegalPageResult[];
+  })
+
+  describe('filterPageLegalPages', () => {
+
+    it('should return a filterd list of lega page nodes', () => {
+      const filterPageLegalPages = filterPageByTitle(legalIds);
+      const pages = shopifyPages.nodes as ShopifyPage[]
+      expect(filterPageLegalPages(pages)).toStrictEqual(legalPageResult)
+    });
+  });
+});
--- packages/store/src/model/types.ts
+++ packages/store/src/model/types.ts
@@ -20,3 +20,16 @@ export interface ProductImageProps {
     readonly name?: string;
   };
 }
+
+export interface ShopifyPage {
+  readonly body: string,
+  readonly handle: string,
+  readonly id: string,
+  readonly shopifyId: string,
+  readonly slug: string,
+  readonly title: string,
+  readonly updatedAt: string,
+  readonly bodySummary: string,
+  readonly url: string,
+  readonly parent: Node
+}
