43b7f5ce831f189d673709d40f734b56b3ae511c
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2021-04-24 12:56:29 +0200

    feat: provide growing font-sizes for the Text component

--- packages/components/src/__tests__/Text.test.tsx
+++ packages/components/src/__tests__/Text.test.tsx
@@ -1,102 +1,120 @@
-import React from 'react';
-import { ThemeProvider } from 'emotion-theming';
-import { axe, toHaveNoViolations } from 'jest-axe';
-import { matchers } from 'jest-emotion';
-import { render, cleanup } from '@testing-library/react';
-import { theme, Text } from '..';
+import { Text, theme } from ".."
+import { axe, toHaveNoViolations } from "jest-axe"
+import { cleanup, render } from "@testing-library/react"
 
-expect.extend(matchers);
-expect.extend(toHaveNoViolations);
+import React from "react"
+import { ThemeProvider } from "emotion-theming"
+import { matchers } from "jest-emotion"
 
-describe('<Text />', () => {
-  let testId: string;
+expect.extend(matchers)
+expect.extend(toHaveNoViolations)
+
+describe("<Text />", () => {
+  let testId: string
   beforeEach(() => {
-    testId = 'test-text';
-  });
+    testId = "test-text"
+  })
 
   afterEach(() => {
-    testId = (undefined as unknown) as string;
-  });
+    testId = (undefined as unknown) as string
+  })
 
-  it('should not have accessibility violations', async done => {
+  it("should not have accessibility violations", async done => {
     const { container, unmount } = render(
       <ThemeProvider theme={theme}>
         <Text size="s">{testId}</Text>
       </ThemeProvider>,
-    );
+    )
 
-    const a11yResults = await axe(container);
-    expect(a11yResults).toHaveNoViolations();
-    cleanup();
-    unmount();
-    done();
-  });
+    const a11yResults = await axe(container)
+    expect(a11yResults).toHaveNoViolations()
+    cleanup()
+    unmount()
+    done()
+  })
 
-  describe('Size', () => {
-    it('should have the size S font-size', () => {
+  describe("Size", () => {
+    it("should have the size S font-size", () => {
       const { getByText, unmount } = render(
         <ThemeProvider theme={theme}>
           <Text size="s">{testId}</Text>
         </ThemeProvider>,
-      );
-      const text = getByText(testId);
-      expect(text).toHaveStyleRule('font-size', `${theme.text.s.fontSize}px`);
-      unmount();
-    });
+      )
+      const text = getByText(testId)
+      expect(text).toHaveStyleRule("font-size", `${theme.text.s.fontSize}px`)
+      unmount()
+    })
 
-    it('should have the size M font-size', () => {
+    it("should have the size M font-size", () => {
       const { getByText, unmount } = render(
         <ThemeProvider theme={theme}>
           <Text size="m">{testId}</Text>
         </ThemeProvider>,
-      );
-      const text = getByText(testId);
-      expect(text).toHaveStyleRule('font-size', `${theme.text.m.fontSize}px`);
-      unmount();
-    });
+      )
+      const text = getByText(testId)
+      expect(text).toHaveStyleRule("font-size", `${theme.text.m.fontSize}px`)
+      unmount()
+    })
 
-    it('should have the size L font-size', () => {
+    it("should have the size L font-size", () => {
       const { getByText, unmount } = render(
         <ThemeProvider theme={theme}>
           <Text size="l">{testId}</Text>
         </ThemeProvider>,
-      );
+      )
 
-      const text = getByText(testId);
-      expect(text).toHaveStyleRule('font-size', `${theme.text.l.fontSize}px`);
-      unmount();
-    });
-  });
+      const text = getByText(testId)
+      expect(text).toHaveStyleRule("font-size", `${theme.text.l.fontSize}px`)
+      unmount()
+    })
+  })
 
-  describe('Font Family', () => {
-    it('should have the legend variant font-family', () => {
+  describe("Font Family", () => {
+    it("should have the legend variant font-family", () => {
       const { getByText, unmount } = render(
         <ThemeProvider theme={theme}>
           <Text size="s">{testId}</Text>
         </ThemeProvider>,
-      );
-      const legend = getByText(testId);
+      )
+      const legend = getByText(testId)
       expect(legend).toHaveStyleRule(
-        'font-family',
-        theme.text.fontFamily.replace(/\s/g, ''),
-      );
-      unmount();
-    });
-  });
+        "font-family",
+        theme.text.fontFamily.replace(/\s/g, ""),
+      )
+      unmount()
+    })
+  })
 
-  describe('Color', () => {
-    it('should have the legend variant color', () => {
+  describe("Color", () => {
+    it("should have the legend variant color", () => {
       const { getByText, unmount } = render(
         <ThemeProvider theme={theme}>
           <Text size="s">{testId}</Text>
         </ThemeProvider>,
-      );
-      const text = getByText(testId);
-      expect(text).toHaveStyleRule(
-        'color',
-        theme.text.color.replace(/\s/g, ''),
-      );
-      unmount();
-    });
-  });
-});
+      )
+      const text = getByText(testId)
+      expect(text).toHaveStyleRule("color", theme.text.color.replace(/\s/g, ""))
+      unmount()
+    })
+  })
+
+  describe("Breakpoint - responsive styles", () => {
+    it("should return an m growSize font-size on passed breakpoint index", () => {
+      const { getByText, unmount } = render(
+        <ThemeProvider theme={theme}>
+          <Text breakpoint={2} size="m">
+            {testId}
+          </Text>
+        </ThemeProvider>,
+      )
+
+      const text = getByText(testId)
+      const media = `(min-width: ${theme.breakpoints[2]})`
+
+      expect(text).toHaveStyleRule("font-size", `${theme.text.m.growSize}px`, {
+        media,
+      })
+      unmount()
+    })
+  })
+})
