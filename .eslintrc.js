9323b95d90f75929c6c71be3d4954fd1449ff497
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2021-08-08 11:38:06 +0200

    fix: WIP

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,9 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
-  plugins: ["react-hooks"],
+  plugins: ["react-hooks", "@emotion"],
   extends: [
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
     "prettier",
   ],
   parserOptions: {
@@ -24,6 +23,16 @@ module.exports = {
         args: "after-used",
         ignoreRestSiblings: false,
       },
+    ],
+    "react/prop-types": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/explicit-module-boundary-types": [
+      "off",
+      { allowExpressions: true },
+    ],
+    "@typescript-eslint/explicit-function-return-type": [
+      "off",
+      { allowExpressions: true },
     ],
   },
   settings: {
