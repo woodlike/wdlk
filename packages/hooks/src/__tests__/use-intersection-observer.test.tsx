651de4c3115d466a48b4af8cdf61f6d99f3005c9
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2021-08-16 21:09:23 +0200

    fix: update hooks package formatting style

--- packages/hooks/src/__tests__/use-intersection-observer.test.tsx
+++ packages/hooks/src/__tests__/use-intersection-observer.test.tsx
@@ -1,32 +1,29 @@
-import React from 'react';
-import { act, render } from '@testing-library/react';
-import {
-  calcThreshold,
-  IntersectionOptions,
-  useIntersectionObserver,
-} from '..';
+import { IntersectionOptions, calcThreshold, useIntersectionObserver } from ".."
+import { act, render } from "@testing-library/react"
+
+import React from "react"
 
 const Component: React.FC = () => {
   const [setRef, entry] = useIntersectionObserver<HTMLDivElement>({
     threshold: calcThreshold(10),
-  });
+  })
 
   return (
     <div data-testid="test-component" ref={(el: HTMLDivElement) => setRef(el)}>
       {entry && entry.intersectionRatio}
     </div>
-  );
-};
+  )
+}
 
-describe('useIntersectionObserver()', () => {
-  let instances: Map<Element, Partial<IntersectionObserver>>;
-  let callbacks: Map<Element, (entry: IntersectionObserverEntry[]) => void>;
+describe("useIntersectionObserver()", () => {
+  let instances: Map<Element, Partial<IntersectionObserver>>
+  let callbacks: Map<Element, (entry: IntersectionObserverEntry[]) => void>
 
-  describe('useIntersectionObserver hook internals', () => {
+  describe("useIntersectionObserver hook internals", () => {
     beforeEach(() => {
-      instances = new Map();
-      callbacks = new Map();
-      Object.defineProperty(window, 'IntersectionObserver', {
+      instances = new Map()
+      callbacks = new Map()
+      Object.defineProperty(window, "IntersectionObserver", {
         writable: true,
         value: jest.fn((cb, options: IntersectionOptions) => {
           const instance = {
@@ -36,41 +33,41 @@ describe('useIntersectionObserver()', () => {
             root: options.root,
             rootMargin: options.rootMargin,
             observe: jest.fn((element: Element) => {
-              callbacks.set(element, cb);
-              instances.set(element, instance);
+              callbacks.set(element, cb)
+              instances.set(element, instance)
             }),
             disconnect: jest.fn(),
-          };
-          return instance;
+          }
+          return instance
         }),
-      });
-    });
+      })
+    })
 
     afterEach(() => {
-      ((window.IntersectionObserver as unknown) as Partial<
+      ;((window.IntersectionObserver as unknown) as Partial<
         IntersectionObserver
       > & {
-        mockClear: () => void;
-      }).mockClear();
-      instances = (undefined as unknown) as Map<Element, IntersectionObserver>;
-      callbacks = (undefined as unknown) as Map<Element, () => void>;
-    });
+        mockClear: () => void
+      }).mockClear()
+      instances = (undefined as unknown) as Map<Element, IntersectionObserver>
+      callbacks = (undefined as unknown) as Map<Element, () => void>
+    })
 
-    it('should have been called with the right DOM reference', () => {
-      const { getByTestId } = render(<Component />);
-      const el = getByTestId('test-component');
-      const instance = instances.get(el) as Partial<IntersectionObserver>;
+    it("should have been called with the right DOM reference", () => {
+      const { getByTestId } = render(<Component />)
+      const el = getByTestId("test-component")
+      const instance = instances.get(el) as Partial<IntersectionObserver>
 
-      expect(instance.observe).toHaveBeenCalledWith(el);
-    });
+      expect(instance.observe).toHaveBeenCalledWith(el)
+    })
 
-    it('should return the IntersectionEntry on intersecting', () => {
-      const { getByTestId } = render(<Component />);
-      const el = getByTestId('test-component');
-      const instance = instances.get(el) as Partial<IntersectionObserver>;
+    it("should return the IntersectionEntry on intersecting", () => {
+      const { getByTestId } = render(<Component />)
+      const el = getByTestId("test-component")
+      const instance = instances.get(el) as Partial<IntersectionObserver>
       const callback = callbacks.get(el) as (
         entry: Partial<IntersectionObserverEntry>[],
-      ) => void;
+      ) => void
       const entry = [
         {
           boundingClientRect: el.getBoundingClientRect(),
@@ -82,21 +79,21 @@ describe('useIntersectionObserver()', () => {
             : {},
           target: el,
         } as Partial<IntersectionObserverEntry>,
-      ];
-      expect(el.innerHTML).toBe('');
-      act(() => callback(entry));
-      expect(el.innerHTML).toBe('1');
-    });
-  });
+      ]
+      expect(el.innerHTML).toBe("")
+      act(() => callback(entry))
+      expect(el.innerHTML).toBe("1")
+    })
+  })
 
-  describe('calcThreshold()', () => {
-    it('should return an uniform distribution of steps ', () => {
-      expect(calcThreshold(1)).toBe(1.0);
-      expect(calcThreshold(2)).toEqual([0, 0.5]);
-      expect(calcThreshold(3)).toEqual([0, 0.33, 0.67]);
-      expect(calcThreshold(5)).toEqual([0, 0.2, 0.4, 0.6, 0.8]);
-      expect(calcThreshold(10)).toMatchSnapshot();
-      expect(calcThreshold(100)).toMatchSnapshot();
-    });
-  });
-});
+  describe("calcThreshold()", () => {
+    it("should return an uniform distribution of steps ", () => {
+      expect(calcThreshold(1)).toBe(1.0)
+      expect(calcThreshold(2)).toEqual([0, 0.5])
+      expect(calcThreshold(3)).toEqual([0, 0.33, 0.67])
+      expect(calcThreshold(5)).toEqual([0, 0.2, 0.4, 0.6, 0.8])
+      expect(calcThreshold(10)).toMatchSnapshot()
+      expect(calcThreshold(100)).toMatchSnapshot()
+    })
+  })
+})
--- packages/hooks/src/__tests__/use-media.test.ts
+++ packages/hooks/src/__tests__/use-media.test.ts
@@ -1,44 +1,44 @@
-import { renderHook } from '@testing-library/react-hooks';
-import { useMedia } from '..';
+import { renderHook } from "@testing-library/react-hooks"
+import { useMedia } from ".."
 
-describe('useMedia', () => {
+describe("useMedia", () => {
   beforeEach(() => {
-    Object.defineProperty(window, 'matchMedia', {
+    Object.defineProperty(window, "matchMedia", {
       writable: true,
       value: jest.fn().mockImplementation(query => ({
-        matches: query === '(min-width: 1000px)' ? true : false,
+        matches: query === "(min-width: 1000px)" ? true : false,
         media: query,
         addListener: jest.fn(),
         removeListener: jest.fn(),
         dispatchEvent: jest.fn(),
       })),
-    });
-  });
+    })
+  })
 
-  it('should not match any query and return the default value', () => {
+  it("should not match any query and return the default value", () => {
     const queries = [
-      '(min-width: 1500px)',
-      '(min-width: 960px)',
-      '(min-width: 600px)',
-    ];
-    const values = [5, 4, 3];
-    const defaultValue = 666;
+      "(min-width: 1500px)",
+      "(min-width: 960px)",
+      "(min-width: 600px)",
+    ]
+    const values = [5, 4, 3]
+    const defaultValue = 666
 
     const { result } = renderHook(() =>
       useMedia<number>(queries, values, defaultValue),
-    );
-    expect(result.current).toBe(defaultValue);
-  });
+    )
+    expect(result.current).toBe(defaultValue)
+  })
 
-  it('should match the (min-width: 1000px) query and return the second value', () => {
+  it("should match the (min-width: 1000px) query and return the second value", () => {
     const queries = [
-      '(min-width: 1500px)',
-      '(min-width: 1000px)',
-      '(min-width: 600px)',
-    ];
-    const values = [5, 4, 3];
+      "(min-width: 1500px)",
+      "(min-width: 1000px)",
+      "(min-width: 600px)",
+    ]
+    const values = [5, 4, 3]
 
-    const { result } = renderHook(() => useMedia<number>(queries, values, 2));
-    expect(result.current).toBe(values[1]);
-  });
-});
+    const { result } = renderHook(() => useMedia<number>(queries, values, 2))
+    expect(result.current).toBe(values[1])
+  })
+})
