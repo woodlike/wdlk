6526651a2db9b381a413e7fd0d666c3585895809
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2023-05-15 18:36:32 +0200

    feat: support play pause events in video hooks

--- packages/hooks/src/use-video-control.ts
+++ packages/hooks/src/use-video-control.ts
@@ -1,7 +1,9 @@
-import { RefObject, useRef, useState } from "react"
+import React, { RefObject, useRef, useState } from "react"
 
 export interface VideoControlState {
   readonly mute: boolean
+  readonly isPlaying: boolean
+  readonly setIsPlaying: React.Dispatch<React.SetStateAction<boolean>>
   readonly setMute: React.Dispatch<React.SetStateAction<boolean>>
   readonly videoRef: RefObject<HTMLVideoElement>
   readonly play: () => Promise<void>
@@ -11,29 +13,32 @@ export interface VideoControlState {
 export function useVideoControl(muted = true): VideoControlState {
   const videoRef = useRef<HTMLVideoElement>(null)
   const [mute, setMute] = useState<boolean>(muted)
+  const [isPlaying, setIsPlaying] = useState<boolean>(false)
 
   const play = async (): Promise<void> => {
     try {
-      if (!videoRef || !videoRef.current) {
-        return
-      }
+      if (!videoRef || !videoRef.current) return
+
       await videoRef.current.play()
+      setIsPlaying(true)
     } catch (err) {
-      Promise.resolve(console.error(`Video Control Hook [error]: ${err}`))
+      return Promise.reject(`Video Control Hook [error]: ${err}`)
     }
   }
 
   const pause = (): void => {
-    if (!videoRef || !videoRef.current) {
-      return
-    }
+    if (!videoRef || !videoRef.current) return
+
     videoRef.current.pause()
+    setIsPlaying(false)
   }
 
   return {
     videoRef,
     mute,
+    isPlaying,
     setMute,
+    setIsPlaying,
     pause,
     play,
   }
